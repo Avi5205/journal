8c93684b5d18d046d87877377fdcfb2842dcb223
Avinash Kumar <akgpcamp@gmail.com>
2024-07-21 09:58:33 +0530

    project completed with basic CRUD operation

---
+++ src/main/java/in/kodder/journalApp/repository/JournalEntryRepository.java
@@ -0,0 +1,8 @@
+package in.kodder.journalApp.repository;
+
+import in.kodder.journalApp.entity.JournalEntry;
+import org.bson.types.ObjectId;
+import org.springframework.data.mongodb.repository.MongoRepository;
+
+public interface JournalEntryRepository extends MongoRepository<JournalEntry, ObjectId> {
+}
