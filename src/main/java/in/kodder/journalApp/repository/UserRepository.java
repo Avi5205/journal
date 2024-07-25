c675bad7a116babd571e1ed9f0150eb8075306a6
Avinash Kumar <akgpcamp@gmail.com>
2024-07-25 17:36:31 +0530

    adding some codes on journal entries

--- src/main/java/in/kodder/journalApp/repository/UserRepository.java
+++ src/main/java/in/kodder/journalApp/repository/UserRepository.java
@@ -1,5 +1,6 @@
 package in.kodder.journalApp.repository;
 
+import in.kodder.journalApp.entity.JournalEntry;
 import in.kodder.journalApp.entity.UserEntity;
 import org.bson.types.ObjectId;
 import org.springframework.data.mongodb.repository.MongoRepository;
